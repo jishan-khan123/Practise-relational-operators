367f2477984fa0819e7ffa431ad3fce6d7398a50
jishan <jk101793@gmail.com>
2026-01-21 05:31:32 +0530

    loops concept

--- DAY3.java
+++ DAY3.java
@@ -1,39 +1,55 @@
 import java.util.Scanner;
 
 public class day3 {
-  public static void main(String[] args) {
-    
-System.out.println("hello java today we are practise on loops");
-        Scanner sc = new Scanner(System.in);
-       
+    public static void main(String[] args) {
         
-        // int a = 786;
-        // System.out.println(Integer.toBinaryString(a));
-        // find number of power 
-        System.out.println("enter number");
-        int bass = sc.nextInt();
-        System.out.println("enter power number");
-         int power = sc.nextInt();
-
-         int result =1 ;
-
-         for(int i=1; i<=power;i++){
-          result = result * bass ;
+        int a =8;
+        float b= 4.45454f;
+        char k = 'j';
+        String s ="Rajput";
+        double m =54584545; 
+        System.out.format("the value of a is = %d and the value of b is = %f and then value of c is = %c and the value of d is = %s" ,a,b,k,s);
+        Scanner sc =new Scanner(System.in);
+System.out.println("enter age");
+        int age =sc.nextInt();
+        System.out.println("enter marks");
+        int marks = sc.nextInt();
+        if (age<=18 && marks >= 33) {
+            System.out.println("congrats....!");
         }
-        
-        System.out.println("Result = " + result);
+        else{
+            System.out.println("you are not Eligible and try again");
+        }
+       
 
-        
-        
-        
-         
-      }
-  }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(" * ");
+        }
+System.out.println();
+                }
+int i = 0;
+while (i<100) {
 
+System.out.println(i);
+if (i == 50) {
+    break;
+}
+else{
+    System.out.println("baby");
+}
+i++;
 
-
-
-
+}
+System.out.println("hello !");
     
-    
+}
+
+
+
+
+
+    }
 
